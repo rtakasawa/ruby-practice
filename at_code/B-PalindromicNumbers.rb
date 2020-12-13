e43e1dabab0b4859f7d9febf22017c65d6b4e730
ryosuke takasawa <ten.r0619@gmail.com>
2020-12-14 04:59:14 +0900

    add

---
+++ at_code/B-PalindromicNumbers.rb
@@ -0,0 +1,4 @@
+# https://atcoder.jp/contests/abc090/tasks/abc090_b
+
+a,b = gets.chomp.split(' ').map(&:to_i)
+
--- at_code/B-SomeSums.rb
+++ at_code/B-SomeSums.rb
@@ -1,13 +1,45 @@
-n,a,b = gets.split(' ').map(&:to_i)
+# n,a,b = gets.split(' ').map(&:to_i)
 
-total = 0
+# total = 0
 
-(1..n).each do |i|
-  num = i.to_s.split('').map(&:to_i)
-  sum_count = num.inject(:+)
-  if a <= sum_count && sum_count <= b
-    total += i
-  end
+# (1..n).each do |i|
+#   num = i.to_s.split('').map(&:to_i)
+#   sum_count = num.inject(:+)
+#   if a <= sum_count && sum_count <= b
+#     total += i
+#   end
+# end
+
+# puts total
+
+
+N,A,B = gets.chomp.split(' ').map(&:to_i)
+# N以下の整数
+# 各桁の和が、A以上B以下の合計を出力する
+
+def find_sum_of_digits n
+    sum = 0
+    while (n > 0)
+        binding.irb
+        # 10で割った余りを足すのが定石
+        sum += n % 10
+        n /= 10
+    end
+    binding.irb
+    return sum
 end
 
-puts total
+sum = 0
+total = 0
+
+for n in 1..N do
+    # 各桁の和を求める
+    binding.irb
+    sum = find_sum_of_digits n
+    if sum >= A && sum <= B
+        total += n
+    end
+    n += 1
+end
+
+p total
