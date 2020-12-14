1e3a3c01cbd6944a25c7f16c13512c8682066b58
ryosuke takasawa <ten.r0619@gmail.com>
2020-12-15 04:32:54 +0900

    add

--- at_code/B-PalindromicNumbers.rb
+++ at_code/B-PalindromicNumbers.rb
@@ -2,3 +2,14 @@
 
 a,b = gets.chomp.split(' ').map(&:to_i)
 
+sum_count = 0
+
+(a..b).each do |i|
+  # i/1%10で１のくらいの整数を出力できる
+  # i/10%10で10のくらいの整数を出力できる
+  if i/1%10 == i/10000%10 && i/10%10 == i/1000%10
+    sum_count += 1
+  end
+end
+
+puts sum_count
