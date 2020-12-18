2463e75883e23390596d41ec441312350adf1510
ryosuke takasawa <ten.r0619@gmail.com>
2020-12-18 23:32:31 +0900

    add

--- at_code/A-DigitsSum.rb
+++ at_code/A-DigitsSum.rb
@@ -1,16 +1,15 @@
 n = gets.chomp.to_i
 
-smallest = 1000000000
+smallest = 100000
 
 (2..100000).each do |a|
-  b = (n - a).abs
+  b = n - a
   a_total = a/1%10 + a/10%10 + a/100%10 + a/1000%10 + a/10000%10
   b_total = b/1%10 + b/10%10 + b/100%10 + b/1000%10 + b/10000%10
   total = a_total + b_total
-  if total < smallest
-    smallest =+ total
+  if smallest > total
+    smallest =+ a_total + b_total
   end
-  binding.irb
 end
 
 puts smallest
